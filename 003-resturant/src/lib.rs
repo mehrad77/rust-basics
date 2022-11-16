6f122e6842e80e419e9cb0022b441bc8a7aeabb2
Mahrad <mehrad@protonmail.com>
2022-11-16 23:16:54 +0330

    add restaurant app

---
+++ 003-resturant/src/lib.rs
@@ -0,0 +1,11 @@
+mod front_of_house;
+
+pub use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
+}
